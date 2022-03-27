e48f2b22d6f9d179847f9ff53a3d91d38c9a57f4
anakochiashvili <102468341+anakochiashvili@users.noreply.github.com>
2022-03-27 19:21:24 +0400

    Add files via upload

---
+++ დავალება #1/davaleba.js
@@ -0,0 +1,155 @@
+/* მოგესალმებით მეგობრებო ეს არის დავალება ნომერი 1. დავალების ყველა ეტაპი აღწერილია კომენტარის სახით
+ * 1) კომენტარები
+ * 2) ცვლადები
+ * 3) მონაცემთა ტიპები
+ * 4) ოპერატორები და არითმეტიკა
+ */
+
+
+
+// 1 კომენტარები
+
+/*
+  // კომენტარები გაფუჭდა თქვენი დავალებაა
+  // არასწორად დაწერილი კომენტარი ჩაასწოროთ
+  // და ასევე გამოიყენოთ თქვენი კოდის აღსაწერად
+*/
+
+
+
+// 2 ცვლადები
+
+/*
+  // მსურს ჩემი Spotify ლისტის განახლება 
+  // ამაში კი თქვენი დახმარება მჭირდება
+  // აღწერეთ თქვენი საყვარელი მომღერლის
+  // (სახელი, გვარი, ასაკი, დაბადების წელი, დაბადების ადგილი,)
+  // გაითვალისწინეთ რომ ამისთვის უნდა
+  // გამოიყენოთ მხოლოდ (const, let)
+  // (ნებისმიერი პიროვნების ჩაწერა შეგიძლიათ)
+*/
+
+// >>>> კოდი დაწერეთ აქ
+let Name = " name: Sam ";
+let Surname ="surname: Smith ";
+let age1 ="age: 29";
+let dateofbirth =" date of birth:19 May 1992 ";
+const placeofbirth =" place of birth: London, England ";
+
+console.log(Name)
+console.log(Surname)
+console.log(age1)
+console.log(dateofbirth)
+console.log(placeofbirth)
+
+
+
+
+
+// 
+
+/*
+  // დროა დავიწყოთ მზადება ჩვენს მომავალ პროექტზე.
+  // ჩვენი დავალეებაა მოვიფიქროთ რაიმე პროდუქტთა სია
+  // რომელსაც გამოვიყენებთ მომავალში.
+
+  წიგნების მაღაზიის მაგალითი:
+
+  const BookName = "ვეფხისტყაოსანი";
+  const AuthrName = "შოთა რუსთაველი";
+  const CreateDate = "13 საუკუნე";
+  let ProductQuant = 124;
+*/
+
+
+// >>>> კოდი დაწერეთ აქ
+//------------------------football team------------------//
+const team ="  Barcelona";
+const AuthrName= "Joan Gamper";
+const CreateDate = "November 29, 1899";
+let Price ="$4.76 billion";
+let place ="Spain, Barcelona";
+let Nickname ="Barça or Blaugrana ";
+
+
+console.log(team)
+console.log(AuthrName)
+console.log(CreateDate)
+console.log(Price)
+console.log(place)
+console.log(Nickname)
+
+
+
+
+
+// 3 მონაცემთა ტიპები
+
+/*
+  // აქ რაღაც შეცდომაა თქვენი დავალებაა ცვლადის გამოცხადების
+  // დროს გაასწოროთ შეცდომა;
+*/
+
+
+
+const age = 1;
+const hisName = "bob";
+const isHigh = (true);
+const dateOfBirth = 1995;
+
+
+
+
+// ეს კოდი ამოწმებს თქვენ მიერ მითითებულ ტიპებს
+
+// თქვენი მიზანია დააბრუნოთ პირველი 4 frue
+
+console.log(typeof age === "number");
+console.log(typeof hisName === "string");
+console.log(typeof isHigh === "boolean");
+console.log(typeof dateOfBirth === "number");
+
+
+
+// აქ უნდა გაიგოთ თქვენი პროდუქტის სიის ტიპი
+
+// მაგ: console.log(typeof age)
+
+
+
+// ოპერატორები არითმეტიკა
+
+let num = 5;
+
+
+// გამოიყენეთ ყველა ვარიანტი რომლითაც შეგვიძლია ამ ციფრის გაზრდა
+
+// >>> კოდი დაწერეთ აქ
+
+console.log(num + 3)
+console.log(num * 3)
+console.log(num  **3)
+//
+
+console.log(num)
+
+
+
+// სცადეთ შეკრიბოთ განსხვავებული ტიპის მქონე 
+// მნიშვნელობები და გაიგოთ შეკრებილი მონაცემების ტიპი
+
+// >>> კოდი დაწერეთ აქ
+let number = 15;
+let numb = 8;
+console.log(number + numb)
+console.log(numb + number)
+console.log(number-numb)
+console.log(number /numb)
+console.log(number ** numb)
+
+console.log(typeof number === "string")
+console.log(typeof number === "number")
+
+
+
+//discord  Nickname: ana.kochiashvili
